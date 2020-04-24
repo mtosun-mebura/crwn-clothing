e7d9521df78626e58176956359cd00f5d5b66e71
Mehmet Tosun <m.tosun@mebura.nl>
2020-04-24 13:27:20 +0200

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -20,8 +20,25 @@ class App extends React.Component {
 	unsubscribeFromAuth = null
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async currentUser => {
-			createUserProfileDocument(currentUser);
+		// this.unsubscribeFromAuth = auth.onAuthStateChanged(async currentUser => {
+		// 	createUserProfileDocument(currentUser);
+		// })
+
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot(snapShot => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data()
+						}
+					});
+				})
+			}
+
+			this.setState({ currentUser: userAuth });
 		})
 	}
 
